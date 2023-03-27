586831c06a27e3c51e8ee21c7c0e9260978d79b5
Userfound404 <21071A7204@vnrvjiet.in>
2023-03-27 13:17:59 +0530

    home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+// import {logo} from './assets'
+// import { Home, Resources , Lab } from './page';
+import Home from '../page/Home';
+import Resources from '../page/Resources';
+import Lab from '../page/Lab';
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-5 py-4 border-b border-b-[#e6ebf4]">
+      <Link to="/">
+        {/* <img src={logo} alt="logo" className="w-14 object-contain" /> */}
+      </Link>
+      <ul>
+      <li><Link to="/resources" className="font-inter font-medium bg-[#3BB143] text-white px-4 py-2 rounded-md"> View Resources </Link></li>
+      <li><Link to="/lab" className="font-inter font-medium bg-[#3BB143] text-white px-4 py-2 rounded-md"> Lab test </Link></li>
+      </ul>
+    </header>
+    <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/lab" element={<Lab />} />
+      </Routes>
+    </main>
+    <footer className='w-full flex justify-between items-center bg-white text-s sm:px-8 px-2 py-3 border-b border-b-[#e6ebf4]'>
+    <h6> Team Escape </h6>
+    </footer>
+  </BrowserRouter>
+);
 
-export default App
+export default App;
